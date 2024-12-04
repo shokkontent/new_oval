7e3ab01b8eb29154f4c73e3a4fd2323ead2ce8f1
Dima <grmgorer@gmail.com>
2024-12-05 00:23:46 +0300

    сделал

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import random
+from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        # Загружаем дизайн
+        uic.loadUi("Ui.ui", self)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        a = random.randint(10, 100)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(random.randint(0, 200)
+                       , random.randint(0, 200), a, a)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
